fb13aaf99947f4434d821c4bb3ebe785dd6eb207
Krish Vyas <klzsh5@gmail.com>
2025-10-31 18:38:22 -0400

    Rewrote SDMMC class so that it handles both reads and writes

--- src/RecordData/SDMMC/SDMMCBackend.h
+++ src/RecordData/SDMMC/SDMMCBackend.h
@@ -1,9 +1,11 @@
 #ifndef SDMMC_BACKEND_H
 #define SDMMC_BACKEND_H
 
+#define LINE_LENGTH 512
+
 #if defined(ENV_STM)
 // funny me including this in the namespace :|
-
+#define TAB_SIZE 1
 /*
  *   CAVEATS USING STM32SD
  *   - Will be using 4 bit mode (max speed of about 1 MB/s)
@@ -16,20 +18,30 @@
 // Note that Teensy is not included here because it will never be used with an MMC connection
 namespace astra
 {
+    /*
+    all files will be stored in the root "/" directory. 
+    There is no folder structure, the name of the files will be dictated by the user
+    */
     class SDMMCBackend
     {
-        public:
+    public:
         virtual bool begin();
         virtual bool end();
         virtual bool write(char *data, char *filename);
         virtual bool createFile(char *filename);
-        private:
-        #if defined(ENV_STM)
+        virtual bool deleteFile(char *filename);
+        virtual void listFiles();
+        virtual bool handleChoices();
+        virtual bool readFile(char *filename);
+        virtual bool exists(char *filename);
+
+    private:
+#if defined(ENV_STM)
         File file;
-        #elif defined(ENV_ESP)
-        
-        #endif
+#elif defined(ENV_ESP)
+
+#endif
     };
-   
+
 }
 #endif
