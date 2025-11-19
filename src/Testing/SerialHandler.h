1f10fbd40c6e5266984277f559401de60f3156ec
dsrivas1 <145722611+dsrivas1@users.noreply.github.com>
2025-11-19 18:31:52 -0500

    brought over serial handler
    need serial handler for airbrake data gettign tsent through serial, will take in fake sensor data and spit out updated state using avionics state estimation

---
+++ src/Testing/SerialHandler.h
@@ -0,0 +1,29 @@
+//Divyansh Srivastava 11/19/2025
+//Copied over from old mmfs
+
+
+#include <Arduino.h>
+//#include "../RecordData/Logging/Logger.h" need to change this from old astra 
+
+#define LS_DATE 2
+#define LS_SIZE 4
+
+namespace mmfs
+
+{
+    class SerialHandler
+    {
+        public:
+            void handle();
+            const char *getLastLine() const;
+        private:
+            void fetchList();
+            void clearFiles();
+            void removeFile(char *args);
+            void latestFiles();
+            void copyFile(char *args);
+        private:
+            char line[2500];
+    };
+    SerialHandler &getSerialHandler();
+}
