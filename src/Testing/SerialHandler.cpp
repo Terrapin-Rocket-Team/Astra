9e50738f972e4eb6a29cbdc06b98ae7b564ced8c
dsrivas1 <145722611+dsrivas1@users.noreply.github.com>
2025-12-01 23:14:10 -0500

    using data.h
    so i brought in the encode and decode funcitokns from radio messages for serial parser. used claude to chaneg teh serial handler class to now usign a buffer system becuase we will be handling one file at a time so no point in having file handlers.

--- src/Testing/SerialHandler.cpp
+++ src/Testing/SerialHandler.cpp
@@ -2,113 +2,152 @@
 //Copied over from old MMFS
 
 
+
 #include "SerialHandler.h"
 //#include "Events/Event.h"  need something to replace event's and stuff 
-using namespace mmfs;
 
-void SerialHandler::handle()
-{
-    if (Serial.available())
-    {
-        int i = Serial.readBytesUntil('\n', line, sizeof(line));
-        if (strncmp("cmd/", line, 4) == 0)
-        {
-            line[i] = '\0';
-            char *cmd = strtok(line+4, " ");
-            char *args = strtok(nullptr, "");
-            if (strcmp(cmd, "ls") == 0)
-            {
-                Serial.println("ok ls");
-                fetchList();
-            }
-            else if (strcmp(cmd, "clr") == 0)
-            {
-                Serial.println("ok clr");
-                clearFiles();
-            }
-            else if (strcmp(cmd, "cp") == 0)
-            {
-                Serial.println("ok cp");
-                copyFile(args);
-            }
+#include "SerialHandler.h"
 
-            else if (strcmp(cmd, "rm") == 0)
-            {
-                Serial.println("ok rm");
-                removeFile(args);
+void SerialHandler::handle() {
+    parseSerialStream();
+    
+    if (messageReady) {
+        processCompleteMessage(); //might need to change, this is the step for when it runs the avionics algorithms 
+        messageReady = false;
+    }
+}
+
+void SerialHandler::parseSerialStream() {
+    while (Serial.available() > 0) {
+        uint8_t inByte = Serial.read();
+        
+        // Start of message
+        if (inByte == START_MARKER) {
+            bufferPos = 0;
+        }
+        // End of message - ready to decode
+        else if (inByte == END_MARKER) {
+            uint16_t decoded = decode(buffer, bufferPos);
+            
+            if (decoded > 0) {
+                messageReady = true;
+                // TODO: Add logging here - log successful decode
+                // Logger.log("Decoded %d bytes of sensor data", decoded);
+            } else {
+                // TODO: Add logging here - log decode error
+                // Logger.error("Failed to decode message");
             }
-            else if (strcmp(cmd, "latest") == 0)
-            {
-                Serial.println("ok latest");
-                latestFiles();
-            }
-            else
-            {
-                Serial.printf("no cmd: %s\n", cmd);
+            
+            bufferPos = 0;
+        }
+        // Collect message bytes
+        else {
+            if (bufferPos < sizeof(buffer) - 1) {
+                buffer[bufferPos++] = inByte;
+            } else {
+                // TODO: Add logging here - buffer overflow
+                // Logger.error("Serial buffer overflow");
+                bufferPos = 0;
             }
         }
-        else{
-            getEventManager().invoke(Event("SERIAL_LINE"_i));
-        }
     }
 }
 
-void SerialHandler::fetchList()
-{
-    getLogger().backend->ls(Serial);
-}
-
-void SerialHandler::clearFiles()
-{
-    getLogger().backend->format();
-    Serial.println("Removed All Files");
-}
-
-// expects "fileName_##.csv" i.e. the full name/path of the file.
-void SerialHandler::copyFile(char *args)
-{
-    LoggingBackend *backend = getLogger().backend;
-    if (!backend->exists(args))
-    {
-        Serial.print("File not found: ");
-        Serial.println(args);
-        return;
+uint16_t SerialHandler::decode(uint8_t *data, uint16_t sz) {
+    uint16_t pos = 0;
+    
+    // Check minimum size (13 floats = 52 bytes minimum)
+    if (sz < 52) {
+        return 0;
     }
-    Serial.println("Sending...");
-    LoggingBackendFile *f = backend->open(args);
-    f->seek(0);
-    char c;
-    while (f->readBytes(&c, 1))
-        Serial.write(c);
-    delete f;
+    
+    // Decode all sensor values matching your BlueRaven columns
+    memcpy(&sensorData.batteryVoltage, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.altitude, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.pressure, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.temperature, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.velocity, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.angle, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.accelX, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.accelY, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.accelZ, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.gyroX, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.gyroY, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.gyroZ, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.rollAngle, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    memcpy(&sensorData.tiltAngle, &data[pos], sizeof(float));
+    pos += sizeof(float);
+    
+    // TODO: Add logging here - log decoded values
+    // Logger.log("Alt: %.2f, Temp: %.2f, Vel: %.2f", 
+    //            sensorData.altitude, sensorData.temperature, sensorData.velocity);
+    
+    return pos;  // Return number of bytes decoded
 }
 
-void SerialHandler::removeFile(char *args)
-{
-
-    if (!getLogger().backend->exists(args))
-    {
-        Serial.print("File not found: ");
-        Serial.println(args);
-        return;
-    }
-    getLogger().backend->remove(args);
-    Serial.print("Removed ");
-    Serial.println(args);
+void SerialHandler::processCompleteMessage() {
+    // Process the decoded sensor data
+    // TODO: Add logging here - log to SD card or whatever storage you use
+    // Logger.logSensorData(sensorData);
+    
+    // You can access individual values like:
+    // float currentAlt = sensorData.altitude;
+    // float currentTemp = sensorData.temperature;
 }
 
-// prints out the # of the last power cycle's flight files.
-void SerialHandler::latestFiles()
-{
-    Serial.println(getLogger().getFlightNum() - 1);
-}
-
-SerialHandler &mmfs::getSerialHandler()
-{
-    static SerialHandler SerialHandler;
-    return SerialHandler;
-}
-
-const char *SerialHandler::getLastLine() const{
+const char* SerialHandler::getLastLine() const {
     return line;
+}
+
+// Command handlers (your existing functions)
+void SerialHandler::fetchList() {
+    // TODO: Implement - this would interact with your new logging system
+}
+
+void SerialHandler::clearFiles() {
+    // TODO: Implement - this would interact with your new logging system
+}
+
+void SerialHandler::removeFile(char *args) {
+    // TODO: Implement
+}
+
+void SerialHandler::latestFiles() {
+    // TODO: Implement
+}
+
+void SerialHandler::copyFile(char *args) {
+    // TODO: Implement
+}
+
+// Singleton accessor
+SerialHandler& getSerialHandler() {
+    static SerialHandler instance;
+    return instance;
 }
