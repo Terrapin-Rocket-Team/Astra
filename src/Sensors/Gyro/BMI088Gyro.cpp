499e436d0cda6d6e7e9dadeba14db541dea6b34d
Drew Brandt <ahbrandt@umd.edu>
2025-12-13 04:07:13 -0500

    fixed Gyro Columns

--- src/Sensors/Gyro/BMI088Gyro.cpp
+++ src/Sensors/Gyro/BMI088Gyro.cpp
@@ -4,9 +4,7 @@ namespace astra
 {
     BMI088Gyro::BMI088Gyro(const char *name, TwoWire &bus, uint8_t addr) : Gyro(name), gyro(bus, addr)
     {
-        addColumn("%0.3f", &angVel.x(), "Gyro X (rad/s)");
-        addColumn("%0.3f", &angVel.y(), "Gyro Y (rad/s)");
-        addColumn("%0.3f", &angVel.z(), "Gyro Z (rad/s)");
+
     }
 
     BMI088Gyro::BMI088Gyro(TwoWire &bus, uint8_t addr) : Gyro("BMI088"), gyro(bus, addr)
