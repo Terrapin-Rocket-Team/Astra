71d59d49cd5b269af0e15123ea5fc498e254410c
Krish Vyas <klzsh5@gmail.com>
2026-02-04 16:15:30 -0500

    Added MLX90393 magnetometer

---
+++ src/Sensors/HW/Mag/MLX90393.cpp
@@ -0,0 +1,35 @@
+#include "MLX90393.h"
+
+namespace astra
+{
+
+    MLX90393::MLX90393(const char *name, TwoWire &bus, uint8_t addr) : Mag(name), bus(&bus), addr(addr) {}
+    MLX90393::MLX90393(TwoWire &bus, uint8_t addr) : Mag("MLX90393"), bus(&bus), addr(addr) {}
+    bool MLX90393::init()
+    {
+        if (!magmtr.begin_I2C(addr, bus))
+        {
+            return false;
+        }
+        magmtr.enableAutoRange(true);
+        magmtr.setOversampling(MLX90393_OSR_3);
+        magmtr.setGain(MLX90393_GAIN_1X);
+        magmtr.setResolution(MLX90393_X, MLX90393_RES_19);
+        magmtr.setResolution(MLX90393_Y, MLX90393_RES_19);
+        magmtr.setResolution(MLX90393_Z, MLX90393_RES_19);
+        // this was in the example so im js going with it
+        magmtr.setFilter(MLX90393_FILTER_5);
+        return true;
+    }
+    bool MLX90393::read()
+    {
+        sensors_event_t event;
+        bool status = magmtr.getEvent(&event);
+        mag = {
+            event.magnetic.x,
+            event.magnetic.y,
+            event.magnetic.z,
+        };
+        return status;
+    }
+}
