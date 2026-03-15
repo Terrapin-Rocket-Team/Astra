3f6b486414680a6f9f6918664d5c589cf4ea2466
Drew Brandt <ahbrandt@umd.edu>
2026-03-15 11:18:14 -0400

    Refactor update method in SensorManager to prioritize updating misc sensors before primary sensors

--- src/Sensors/SensorManager/SensorManager.h
+++ src/Sensors/SensorManager/SensorManager.h
@@ -207,7 +207,14 @@ namespace astra
         }
 
         void update(double currentTime)
-        {
+        {            
+            // Update misc sensors first, they can be composite sensors that feed into the primary sensors (e.g. an IMU that contains children)
+            for (uint8_t i = 0; i < numMisc; i++)
+            {
+                if (miscSensors[i] && miscSensors[i]->shouldUpdate(currentTime))
+                    miscSensors[i]->update();
+            }
+            
             // Update primary sensors only if their update interval has elapsed
             if (accel && accel->shouldUpdate(currentTime))
             {
@@ -239,12 +246,7 @@ namespace astra
                 gpsUpdated = true;
             }
 
-            // Update misc sensors
-            for (uint8_t i = 0; i < numMisc; i++)
-            {
-                if (miscSensors[i] && miscSensors[i]->shouldUpdate(currentTime))
-                    miscSensors[i]->update();
-            }
+
         }
 
         /**
