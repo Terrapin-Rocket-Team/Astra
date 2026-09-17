9664ffea29887283a65a82ed1163a142abd472f6
dsrivas1 <145722611+dsrivas1@users.noreply.github.com>
2026-09-16 21:08:02 -0400

    comments

--- src/Filters/LinearKalmanFilter.cpp
+++ src/Filters/LinearKalmanFilter.cpp
@@ -25,27 +25,41 @@ namespace astra
         this->controlSize = K.getRows();
     }
 
+    //In this function, we predict the next state of the system based on the current state and the dynamics of the system
+    //ex. using F = ma to predict where position will be. We use a constant velcity model (getF) and propogate it some 
+    //dt into the future thus geting position. The getG is for the acceleration. Acceleration is an input with matrix U. 
+    //Comes straight from the IM. 
     void LinearKalmanFilter::predictState(double dt, Matrix U)
     {
         X = getF(dt) * X + getG(dt) * U;
     }
-
+    //surprise value; get hte diff between measured and predicted state. Produces the corrected state estimate. 
     void LinearKalmanFilter::estimateState(Matrix measurement)
     {
         X = X + K * (measurement - getH() * X);
     }
-
+    //Kalman ratio = predicted uncertainty / (predicted uncertainty + measurement uncertainty)
+    //Larger P = trust measruements more, larger R = trust prediction more. R is the measruement noise (gps + 
+    //baro noise). GPS noise defaults to 5 and baro noise defaults to 1. No data backs this. 
     void LinearKalmanFilter::calculateKalmanGain()
     {
         K = P * getH().transpose() * (getH() * P * getH().transpose() + getR()).inverse();
     }
-
+    //updates state uncertainity after a measruement. After incorporating the measrumeent to calcualte how uncertain 
+    //the state is. Part f the final part of update step. Done very last
     void LinearKalmanFilter::covarianceUpdate()
     {
         int n = X.getRows();
         P = (Matrix::ident(n) - K * getH()) * P * (Matrix::ident(n) - K * getH()).transpose() + K * getR() * K.transpose();
     }
 
+    // P represents the uncertainity in our state estimation (i.e how confident I am that the predicted state is correct)
+    //We start by propogating the constant velocity (getF) and getting position. The F*P*Ft is the propogation of the old state
+    //uncertainity into the new state uncertainity using ideal dynamics. So if I have P1 at t1 and then I want P2 at t2 I would
+    //this. Again, P is the uncertainity of our state estimate. Q is the uncertainity from our system evolving (i.e. the model is
+    //not perfect and we know that). The Q is taken from 1D model --> 3D model. Q is the uncertainity of the current model (model isn't
+    //perfect). The matrix is a given dt^4/4 via online, but the scaling factor q = process noise shoudl be caculated based on the system.
+    //Right now, q is 0.01 for position (we assume position dynamic is good) and 1.0 for velocity (we assume velocity model is bad)
     void LinearKalmanFilter::covarianceExtrapolate(double dt)
     {
         P = getF(dt) * P * getF(dt).transpose() + getQ(dt);
@@ -57,7 +71,8 @@ namespace astra
         predictState(dt, control);
         covarianceExtrapolate(dt);
     }
-
+    //update, now this is where we taking into account actual measurements. Takes in GPS and baro measuremenets 
+    //1 to use the measurement (GPS gives px, py. Baro gives pz) and 0 to not use the measurement.
     void LinearKalmanFilter::update(Matrix measurement)
     {
         calculateKalmanGain();
